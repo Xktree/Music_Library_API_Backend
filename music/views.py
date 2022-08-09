e694aef8ffb42a9b01781152202778ebd074fd73
Devin Chen <taunwe79@gmail.com>
2022-08-09 08:09:40 -0500

    added imports for views.py

--- music/views.py
+++ music/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from .models import Music 
+from .serializers import MusicSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response 
+from rest_framework import status 
 
 # Create your views here.
