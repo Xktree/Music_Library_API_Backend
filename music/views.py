255df536f50d1ffdcf5270cb6f384936c1c9f4ba
Devin Chen <taunwe79@gmail.com>
2022-08-09 08:11:50 -0500

    created the MusicList class and added the get and post functions

--- music/views.py
+++ music/views.py
@@ -6,3 +6,17 @@ from rest_framework.response import Response
 from rest_framework import status 
 
 # Create your views here.
+
+class MusicList(APIView):
+
+    def get(self, request):
+        music = Music.objects.all()
+        serializer = MusicSerializer(music, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = MusicSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
